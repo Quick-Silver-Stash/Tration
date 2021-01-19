c03d0580dec572217bac378287a521919d9dde15
Seung Ki Lee <seungkilee92@gmail.com>
2021-01-19 18:02:25 -0500

    octocat please hit me up on discord

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,1 +1,11 @@
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+const footerNavigation = createBottomTabNavigator(
+    {
+        Home: Homescreen,
+        Explore: ExploreScreen,
+    },
+    {
+        initialRouteName: 'Home'
+    }
+);
