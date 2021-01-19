1019630d5ce4b1844a703112a1427731646089b3
Seung Ki Lee <seungkilee92@gmail.com>
2021-01-18 19:24:20 -0500

    comment out login on app.js again

--- App.js
+++ App.js
@@ -19,3 +19,109 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// Login Screen Test
+// import { StatusBar } from "expo-status-bar";
+// import React, { useState, Component } from "react";
+// import {
+//   StyleSheet,
+//   Text,
+//   TextInput,
+//   Image,
+//   Button,
+//   TouchableOpacity,
+//   View,
+// } from "react-native";
+
+// export default function App() {
+//         const [email, setEmail] = useState("");
+//         const [password, setPassword] = useState("");
+      
+//         return (
+//           <View style={styles.container}>
+//             <Image
+//               style={styles.image}
+//               source={require("./assets/placeholder-logo-1.png")}
+//             />
+//             <StatusBar style='auto'/>
+//             <View style={styles.inputView}>
+//               <TextInput
+//                 style={styles.TextInput}
+//                 placeholder="Enter Email"
+//                 placeholderTextColor="#003f5c"
+//                 keyboardType="email-address"
+//                 secureTextEntry={false}
+//                 autoCapitalize={false}
+//                 autoCorrect={false}
+//                 onChangeText={(email) => setEmail(email)}
+//                 value={email}
+//               />
+//             </View>
+//             <View style={styles.inputView}>
+//               <TextInput
+//                 style={styles.TextInput}
+//                 placeholder="Enter Password"
+//                 placeholderTextColor="#003f5c"
+//                 secureTextEntry={true}
+//                 autoCapitalize={false}
+//                 autoCorrect={false}
+//                 onChangeText={(password) => setPassword(password)}
+//               />
+//             </View>
+//             <TouchableOpacity>
+//               <Text style={styles.forgot_button}>Forgot Password?</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity>
+//               <Text style={styles.register_button}>Don't have an account yet?</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity style={styles.loginButton}>
+//               <Text style={styles.loginText}>LOGIN</Text>
+//             </TouchableOpacity>
+//           </View>
+//         );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   image: {
+//     width: 200,
+//     height: 150,
+//     marginBottom: 30,
+//   },
+//   inputView: {
+//     backgroundColor: "#1A8BFF",
+//     borderRadius: 30,
+//     width: "70%",
+//     height: 45,
+//     marginBottom: 20,
+//     alignItems: "center",
+//   },
+//   TextInput: {
+//     height: 50,
+//     flex: 1,
+//     padding: 10,
+//     marginLeft: 20,
+//   },
+//   forgot_button: {
+//     height: 30,
+//     marginBottom: 0,
+//   },
+//   register_button: {
+//     height: 30,
+//     marginBottom: 5,
+//   },
+//   loginButton: {
+//     width: "80%",
+//     borderRadius: 25,
+//     height: 50,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginTop: 40,
+//     backgroundColor: "#0068D2",
+//   },
+// });
--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -10,8 +10,7 @@ import {
   View,
 } from "react-native";
 
-export default class Login extends Component {
-    render() {
+export default function App() {
         const [email, setEmail] = useState("");
         const [password, setPassword] = useState("");
       
@@ -19,35 +18,44 @@ export default class Login extends Component {
           <View style={styles.container}>
             <Image
               style={styles.image}
-              source={require("./asset/placeholder-logo-1.png")}
+              source={require("./assets/placeholder-logo-1.png")}
             />
             <StatusBar style='auto'/>
             <View style={styles.inputView}>
               <TextInput
                 style={styles.TextInput}
-                placeholder="Email."
+                placeholder="Enter Email"
                 placeholderTextColor="#003f5c"
+                keyboardType="email-address"
+                secureTextEntry={false}
+                autoCapitalize={false}
+                autoCorrect={false}
                 onChangeText={(email) => setEmail(email)}
+                value={email}
               />
             </View>
             <View style={styles.inputView}>
               <TextInput
                 style={styles.TextInput}
-                placeholder="Password."
+                placeholder="Enter Password"
                 placeholderTextColor="#003f5c"
                 secureTextEntry={true}
+                autoCapitalize={false}
+                autoCorrect={false}
                 onChangeText={(password) => setPassword(password)}
               />
             </View>
             <TouchableOpacity>
               <Text style={styles.forgot_button}>Forgot Password?</Text>
             </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.register_button}>Don't have an account yet?</Text>
+            </TouchableOpacity>
             <TouchableOpacity style={styles.loginButton}>
               <Text style={styles.loginText}>LOGIN</Text>
             </TouchableOpacity>
           </View>
         );
-    }
 }
 
 const styles = StyleSheet.create({
@@ -58,10 +66,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   image: {
-    marginBottom: 40,
+    width: 200,
+    height: 150,
+    marginBottom: 30,
   },
   inputView: {
-    backgroundColor: "#FFC0CB",
+    backgroundColor: "#1A8BFF",
     borderRadius: 30,
     width: "70%",
     height: 45,
@@ -76,7 +86,11 @@ const styles = StyleSheet.create({
   },
   forgot_button: {
     height: 30,
-    marginBottom: 30,
+    marginBottom: 0,
+  },
+  register_button: {
+    height: 30,
+    marginBottom: 5,
   },
   loginButton: {
     width: "80%",
@@ -85,6 +99,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginTop: 40,
-    backgroundColor: "#FF1493",
+    backgroundColor: "#0068D2",
   },
 });
