725c51b9c28c68e1e88e67e7a43b2fe105d47ea4
Seung Ki Lee <seungkilee92@gmail.com>
2021-01-22 15:24:24 -0500

    finalized navigation footer frame

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createAppContainer } from 'react-navigation';
+import Icon from "react-native-vector-icons/FontAwesome";
 import Quests from './src/screens/Quests/Quests';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {
-        /*
-        Put our designs here for now while we don't have a nav bar
-        <Quests />
-        */
-      }
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+        <AppContainer />
+    );
+  }
 }
+
+// Temp Screens
+class LandingScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Temporary Landing Screen </Text>
+      </View>
+    );
+  }
+}
+
+class SearchScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Temporary Search Screen </Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Temporary Profile Screen </Text>
+      </View>
+    );
+  }
+}
+
+class QuestScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Temporary Quest Screen </Text>
+      </View>
+    );
+  }
+}
+
+const bottomTabNavigator = createBottomTabNavigator(
+  {
+    Landing: {
+      screen: LandingScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="home" size={25} color={tintColor} />
+        )
+      }
+    },
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="search" size={25} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="user" size={25} color={tintColor} />
+        )
+      }
+    },
+    Quest: {
+      screen: QuestScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="bars" size={25} color={tintColor} />
+        )
+      }
+    },
+  },
+  {
+    initialRouteName: 'Landing',
+    tabBarOptions: {
+      activeTintColor: '#eb6e3d'
+    }
+  }
+);
+
+const AppContainer = createAppContainer(bottomTabNavigator);
 
 const styles = StyleSheet.create({
   container: {
